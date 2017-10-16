29f8ef43e034fad384902c5e7a856d5f5f28537b
arun789 <adwiti1975@gmail.com>
2017-10-15 19:10:17 -0700

    ways of defining arrays

---
+++ src/MethodCall/WaysOfDefinignArray.scala
@@ -0,0 +1,11 @@
+package MethodCall
+
+object WaysOfDefinignArray {
+  def main(args : Array[String]){
+    var nM = Array("one","two","three")
+    println(nM.apply(0));
+    
+    var _nam = Array.apply("one","two","three");
+    print(_nam.apply(2));
+  }
+}
