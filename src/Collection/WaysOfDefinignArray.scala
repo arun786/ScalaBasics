8d3ddc0a55f184ebdbb5b7038d978b4a4053f7ea
arun789 <adwiti1975@gmail.com>
2017-10-22 13:20:52 -0700

    difference between update and apply

--- src/Collection/WaysOfDefinignArray.scala
+++ src/Collection/WaysOfDefinignArray.scala
@@ -3,9 +3,24 @@ package Collection
 object WaysOfDefinignArray {
   def main(args : Array[String]){
     var nM = Array("one","two","three")
-    println(nM.apply(0));
+    println(nM.apply(0)); //one
     
     var _nam = Array.apply("one","two","three");
-    print(_nam.apply(2));
+    println(_nam.apply(2)); //three
+    
+    _nam(1) = "Four"
+    println(_nam(1)) //Four
+    
+    println(_nam.apply(1)) //Four
+    
+    _nam.update(1, "Five");
+    
+    println(_nam(1)) //Five
+    
+    println(_nam.apply(1)) //Five
+    
+    
+    
+    
   }
 }
