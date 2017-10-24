8e9a36a14476e0a6a2e50de020c6a5c410dc2e49
arun789 <adwiti1975@gmail.com>
2017-10-24 00:22:28 -0700

    List are Homogenous

---
+++ src/Collection/ListAreHomogenous.scala
@@ -0,0 +1,13 @@
+package Collection
+
+object ListAreHomogenous {
+  def main(args : Array[String]){
+    println(1 :: Nil);  //lst: List[Int] = List(1)
+    println(true :: 1 :: Nil) //List[AnyVal] = List(true, 1)
+    println("hello" :: true :: 1 :: Nil) //List[Any] = List(Hello, true, 1)
+    
+    /*Any 
+    AnyVal
+    Int */
+  }
+}
