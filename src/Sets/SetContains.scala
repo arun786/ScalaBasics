ced9f9a025f855307cc147c774967b76eddabea0
arun789 <adwiti1975@gmail.com>
2017-10-25 23:25:55 -0700

    Set Contains examined

---
+++ src/Sets/SetContains.scala
@@ -0,0 +1,17 @@
+package Sets
+
+object SetContains {
+ 
+  def main(args:Array[String]){
+    
+    val fruit = Set("Apple","Banana","Orange");
+    
+    println(fruit contains "Apple"); //true
+    
+    println(fruit("Apple")); //true
+    
+    println(fruit contains "Potato"); //false
+    
+    println(fruit("Potata")); //false
+  }
+}
