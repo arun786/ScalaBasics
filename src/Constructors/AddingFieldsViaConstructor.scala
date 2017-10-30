9959902367d773e53e8e264f82abb57adcf3fcb0
arun789 <adwiti1975@gmail.com>
2017-10-30 00:01:49 -0700

    Adding Fields

---
+++ src/Constructors/AddingFieldsViaConstructor.scala
@@ -0,0 +1,40 @@
+package Constructors
+
+import java.util.Scanner
+
+object AddingFieldsViaConstructor extends App {
+
+  println("Enter the first numerator");
+  var firstnum = new Scanner(System.in);
+  var num = firstnum.nextInt();
+  println("Enter the first Denominator");
+  var firstdenom = new Scanner(System.in);
+  var den = firstdenom.nextInt();
+
+  var first = new Divide(num, den);
+
+  println("Enter the Second numerator");
+  firstnum = new Scanner(System.in);
+  num = firstnum.nextInt();
+  println("Enter the Second Denominator");
+  firstdenom = new Scanner(System.in);
+  den = firstdenom.nextInt();
+
+  var second = new Divide(num, den);
+  var result = first.add(second);
+  println(result);
+
+}
+
+class Divide(n: Int, d: Int) {
+  require(n != 0, "d cannot be zero");
+
+  override def toString: String = n + "/" + d;
+
+  var numer: Int = n;
+  var denom: Int = d;
+
+  def add(that: Divide): Divide = {
+    new Divide(numer * that.denom + denom * that.numer, denom * that.denom);
+  }
+}
