b85983394de3291e7793632f5c68f82508c21802
arun789 <adwiti1975@gmail.com>
2017-11-04 18:37:47 -0700

    Efficient Fibonacci series

--- src/ControlStructures/FibonnaciSeries.scala
+++ src/ControlStructures/FibonnaciSeries.scala
@@ -1,21 +1,23 @@
 package ControlStructures
 
+import java.math.BigInteger
+
 object FibonnaciSeries extends App {
 
-  def fn(num: Int): Array[Integer] = {
+  def fn(num: Int): Array[BigInteger] = {
 
-    var fbs = new Array[Integer](num);
+    var fbs = new Array[BigInteger](num);
 
     var fb = for (i <- 0 until num) yield {
       if (i <= 2) {
-        fbs(i) = i;
+        fbs(i) = BigInteger.valueOf(i);
       } else {
-        fbs(i) = fbs(i - 1) + fbs(i - 2);
+        fbs(i) = fbs(i - 1).add(fbs(i - 2));
       }
 
     }
     fbs;
   }
 
-  println(fn(6)(5)); //8
+  println(fn(50)(49)); //12586269025
 }
