b6eedd3de5d9ebc44dc81cb3874d6a35ca781cd2
arun789 <adwiti1975@gmail.com>
2017-11-07 01:03:22 -0700

    functions as parameters

--- src/Functions/PassingFunctionsAsParameters.scala
+++ src/Functions/PassingFunctionsAsParameters.scala
@@ -3,6 +3,22 @@ package Functions
 object PassingFunctionsAsParameters extends App {
 
   var lstNum = List(1, 2, 3, 4, 5, 6);
-  lstNum.foreach((x: Int) => println(x));
-
+  lstNum.foreach((x: Int) => print(x + " ")); //1 2 3 4 5 6
+  
+  println();
+  lstNum.foreach(x => print(x + " ")); // 1 2 3 4 5 6
+  
+  println();
+  lstNum.filter(x => x > 1).foreach(u => print(u + " ")); //2 3 4 5 6
+  
+  println();
+  lstNum.filter(_ > 2).foreach(u => print(u + " ")); // 3 4 5 6
+  
+  println();
+  var sum = (x : Int, y : Int) => x + y;
+  println(sum(10,20));
+  
+  var diff = (_ : Int) - (_ :Int);
+  println(diff(20,10));
+  
 }
