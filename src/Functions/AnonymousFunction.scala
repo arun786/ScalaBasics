1fd98068fc14d02d0b059fceed618ff4b7fd6aaa
arun789 <adwiti1975@gmail.com>
2017-11-07 00:25:12 -0700

    Anonymous Function

--- src/Functions/AnonymousFunction.scala
+++ src/Functions/AnonymousFunction.scala
@@ -3,7 +3,7 @@ package Functions
 object AnonymousFunction extends App{
   
   
-  //Anonymous Function
+  //Anonymous Function, here we are assigning functions to a variable
   var sum = (x : Int) => {
     x + 10;
   }
